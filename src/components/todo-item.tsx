1f2355322f9ec2c6769f28608fee118c7ce6d29f
Jose Ripoll <jose.ripoll@acrismatic.es>
2023-07-13 16:56:18 +0200

    styles

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -11,10 +11,10 @@ interface Props {
 export const TodoItem = ({ todo }: Props) => {
     return (
         <div key={todo.id} className="flex gap-5 items-center justify-between my-5">
-            <div className="flex gap-3 items-start">
+            <div className="flex gap-3 items-center w-full">
                 <InputDone id={todo.id} isDone={todo.done} />
-                <div className={`${todo.done && 'line-through opacity-40'}`}>
-                    <span className="text-sm text-pink-600">{todo.title}</span>
+                <div className={`${todo.done && 'line-through opacity-40'} border border-gray-600 rounded-xl px-5 py-2 w-full`}>
+                    <span className="text-sm text-sky-400 font-bold">{todo.title}</span>
                     <p className="opacity-50 text-xs">{todo.description}</p>
                     <small className="text-gray-400">{dateAgo(todo.createdAt)}</small>
                 </div>

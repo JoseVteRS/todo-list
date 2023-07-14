dda44bbb8b1802cede20d6bd19950559189ca8eb
Jose Ripoll <jose.ripoll@acrismatic.es>
2023-07-14 13:27:36 +0200

    remove buttons

--- src/components/input-done.tsx
+++ src/components/input-done.tsx
@@ -30,7 +30,7 @@ export const InputDone = ({ id, isDone }: Props) => {
                     checked={isChecked}
                     onChange={toggleCheckboxChange}
                 />
-                <div className={`w-8 h-8 ${isChecked ? 'bg-green-500 border-green-800' : 'border-gray-400'} rounded-lg grid place-content-center border-2  flex-shrink-0 p-1 transition duration-300 ease-in-out mr-2`}>
+                <div className={`w-8 h-8 ${isChecked ? 'bg-green-500 border-green-800' : 'border-gray-400'} rounded-lg grid place-content-center border-2  flex-shrink-0 p-1 transition duration-200 ease-in-out mr-2`}>
                     {isChecked && (
                         <svg className="w-4 h-4 text-gray-100 fill-current" viewBox="0 0 20 20">
                             <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
